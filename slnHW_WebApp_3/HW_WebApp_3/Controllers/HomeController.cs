768c12484f74e42c11a9381af4c83512e28ab1d7
Gerasim1974 <109235581+Gerasim1974@users.noreply.github.com>
2022-09-09 18:26:20 +0300

    web_3

---
+++ slnHW_WebApp_3/HW_WebApp_3/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using HW_WebApp_3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HW_WebApp_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private static readonly FishViewModel[] fish = new[]
+        {
+            new FishViewModel
+            {
+                Name = "Судак",
+                Weight = 3,
+                Speed = 12
+            },
+            new FishViewModel
+            {
+                Name = "Карась",
+                Weight = 1,
+                Speed = 10
+            },
+            new FishViewModel
+            {
+                Name = "Сазан",
+                Weight = 5,
+                Speed = 8
+            }
+        };
+
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult FishByViewData()
+        {
+            ViewData["Fish"] = fish;
+            return View();
+        }
+
+        public IActionResult FishByViewBag()
+        {
+            ViewBag.Fish = fish;
+            return View();
+        }
+
+        public IActionResult FishByModel()
+        {
+            FishViewModel myFish = new FishViewModel
+            {
+                Name = "Карп",
+                Weight = 5,
+                Speed = 8
+            };
+
+            ViewData["myFish"] = myFish;
+            return View(fish);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ slnHW_WebApp_3/HW_WebApp_3/Models/FishViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HW_WebApp_3.Models
+{
+    public class FishViewModel
+    {
+        public string Name { get; set; }   
+        public int Weight { get; set; }
+        public int Speed { get; set; } 
+    }
+}
